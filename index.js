13980e74ad06ec7ba913bf72afefe2bbfbb1db40
Minli <minli.chai@gmail.com>
2024-04-20 21:14:59 +0800

    part c

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
    res.send('Hello World!')
 });
+
+app.post('/', (req, res) => {
+    res.send(req.body)
+ });
 
 app.listen(port, () => {
    console.log(`Server listening at http://localhost:${port}`);
