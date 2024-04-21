38fdb25bbd14033c9dd8f86b3df6841a7f9a29b0
Minli <minli.chai@gmail.com>
2024-04-21 14:32:09 +0800

    connect to mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -15,3 +16,30 @@ app.post('/', (req, res) => {
 app.listen(port, () => {
    console.log(`Server listening at http://localhost:${port}`);
 });
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://minli:minli0327@cluster0.qisckua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
